edc8a993bdc339b144c7450733d703a44bcbc865
berry3 <berry3@indiana.edu>
2021-10-17 17:58:58 -0400

    updating takeString function

--- src/main/scala/dev/ligature/gaze/Steps.scala
+++ src/main/scala/dev/ligature/gaze/Steps.scala
@@ -1,100 +1,180 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 package dev.ligature.gaze
 
-/**
- * stringNibbler matches a given String entirely
- */
-@OptIn(ExperimentalUnsignedTypes::class)
-fun stringNibbler(toMatch: String) = Step { lookAhead ->
-    var offset = 0U
-    var result: State? = null
-    while (offset.toInt() <= toMatch.length) {
-        if (offset.toInt() == toMatch.length - 1 && toMatch[offset.toInt()] == lookAhead.peek(offset)) {
-            result = Complete(offset.toInt() + 1)
-            break
-        } else if (offset.toInt() < toMatch.length && toMatch[offset.toInt()] == lookAhead.peek(offset)) {
-            offset++
-        } else {
-            result = Cancel
-            break
-        }
-    }
-    result ?: Cancel
-}
+import dev.ligature.gaze.Gaze
 
-/**
- * charNibbler matches input against the passed characters
- */
-@OptIn(ExperimentalUnsignedTypes::class)
-fun charNibbler(vararg chars: Char) = Step { lookAhead ->
-    var offset = 0U
-    while (lookAhead.peek(offset) != null) {
-        var match = false
-        chars.forEach check@{
-            if (it == lookAhead.peek(offset)) {
-                match = true
-                return@check
+import scala.util.control.Breaks.break
+
+trait NoMatch
+object NoMatch extends NoMatch
+
+def takeString(toMatch: String): (gaze: Gaze[Char]) => Either[NoMatch, String] = {
+    //    let graphemes = to_match.graphemes(true).collect::<Vec<&str>>();
+    val chars = toMatch.toVector
+    return (gaze) => {
+        var offset = 0
+        var matched = true
+        while (offset < chars.length) {
+            val nextChar = gaze.next()
+            nextChar match {
+                case Some(c) => {
+                    if (chars(offset) == c) {
+                        offset += 1;
+                    } else {
+                        matched = false
+                        break
+                    }
+                }
+                case None => {
+                    matched = false
+                    break
+                }
             }
         }
-        if (!match) {
-            break
+        if (matched) {
+            Right(toMatch)
         } else {
-            offset++
+            Left(NoMatch)
         }
-    }
-    if (offset == 0U) {
-        Cancel
-    } else {
-        Complete(offset.toInt())
     }
 }
 
-/**
- * rangeNibbler matches characters that exist within a given set of CharRanges
- */
-@OptIn(ExperimentalUnsignedTypes::class)
-fun rangeNibbler(vararg ranges: CharRange) = Step { lookAhead ->
-    var offset = 0U
-    while (lookAhead.peek(offset) != null) {
-        var match = false
-        ranges.forEach check@{
-            if (it.contains(lookAhead.peek(offset))) {
-                match = true
-                return@check
-            }
-        }
-        if (!match) {
-            break
-        } else {
-            offset++
-        }
-    }
-    if (offset == 0U) {
-        Cancel
-    } else {
-        Complete(offset.toInt())
-    }
-}
+//  pub fn ignore_all<'a>(
+//      to_match: Vec<&'a str>, //TODO maybe make this an array instead of Vec
+//  ) -> impl Fn(&mut Gaze<&'a str>) -> Result<(), NoMatch> {
+//      move |gaze: &mut Gaze<&'a str>| -> Result<(), NoMatch> {
+//          while !gaze.is_complete() {
+//              let peek = gaze.peek();
+//              match peek {
+//                  Some(peek) => {
+//                      if to_match.contains(&peek) {
+//                          gaze.next();
+//                      } else {
+//                          return Ok(());
+//                      }
+//                  }
+//                  None => return Ok(()),
+//              }
+//          }
+//          Ok(())
+//      }
+//  }
 
-/**
- * predicateNibbler is helper that checks a single character against a given predicate
- */
-@OptIn(ExperimentalUnsignedTypes::class)
-fun predicateNibbler(fn: (Char?) -> Boolean) = Step { lookAhead ->
-    var offset = 0U
-    while (lookAhead.peek(offset) != null) {
-        if (fn(lookAhead.peek(offset))) {
-            offset++
-        } else {
-            break
-        }
-    }
-    if (offset == 0U) {
-        Cancel
-    } else {
-        Complete(offset.toInt())
-    }
-}
+//export function takeWhile(matcher: (toMatch: string) => boolean): (gaze: Gaze<string>) => Either<NoMatch, string> {
+//    return (gaze: Gaze<string>): Either<NoMatch, string> => {
+//        let res = ""
+//        while(true) {
+//            let peek = gaze.peek();
+//            if (peek.isJust()) {
+//                let peekValue = peek.unsafeCoerce()
+//                if (matcher(peekValue)) {
+//                    gaze.next();
+//                    res += peekValue;
+//                } else if (res.length == 0) {
+//                    return Left({});
+//                } else {
+//                    return Right(res);
+//                }
+//            } else {
+//                if (res.length == 0) {
+//                    return Left({});
+//                } else {
+//                    return Right(res);
+//                }
+//            }
+//        }
+//    }
+//}
+
+//  pub fn take_while<'a, T>(
+//      matcher: &'a dyn Fn(T) -> bool,
+//  ) -> impl Fn(&mut Gaze<T>) -> Result<Vec<T>, NoMatch> + '_
+//  where
+//      T: Copy,
+//  {
+//      move |gaze: &mut Gaze<T>| -> Result<Vec<T>, NoMatch> {
+//          let mut res = Vec::new();
+//          loop {
+//              let next = gaze.next();
+//              match next {
+//                  Some(next) => {
+//                      if matcher(next) {
+//                          res.push(next);
+//                      } else if res.is_empty() {
+//                          return Err(NoMatch);
+//                      } else {
+//                          return Ok(res);
+//                      }
+//                  }
+//                  None => {
+//                      if res.is_empty() {
+//                          return Err(NoMatch);
+//                      } else {
+//                          return Ok(res);
+//                      }
+//                  }
+//              }
+//          }
+//      }
+//  }
+
+// /// A Step that takes values from the String until the predicate passes.
+// pub struct TakeUntil<'a>(pub &'a dyn Fn(&str) -> bool);
+
+// impl Tokenizer<String> for TakeUntil<'_> {
+//     fn attempt(&self, gaze: &mut Gaze) -> Result<String, GazeErr> {
+//         //TODO this will need to be rewritten once handle Unicode better
+//         //TODO also this should share code with TakeWhile
+//         let mut res = String::new();
+//         loop {
+//             let next_value = gaze.peek();
+//             match next_value {
+//                 None => {
+//                     return Ok(res);
+//                 }
+//                 Some(c) => {
+//                     if !self.0(c) {
+//                         res += c;
+//                         gaze.next();
+//                     } else {
+//                         return Ok(res);
+//                     }
+//                 }
+//             }
+//         }
+//     }
+// }
+
+// pub struct TakeFirst<'a, T>(pub Box<[&'a dyn Tokenizer<T>]>);
+
+// impl<T> Tokenizer<T> for TakeFirst<'_, T> {
+//     fn attempt(&self, gaze: &mut Gaze) -> Result<T, GazeErr> {
+//         for step in &*self.0 {
+//             let res = gaze.run(*step);
+//             match res {
+//                 Ok(_) => return res,
+//                 Err(_) => continue,
+//             }
+//         }
+//         Err(GazeErr::NoMatch)
+//     }
+// }
+
+// pub struct TakeAll<'a, T>(pub Box<[&'a dyn Tokenizer<T>]>);
+
+// impl<T> Tokenizer<Box<[T]>> for TakeAll<'_, T> {
+//     fn attempt(&self, gaze: &mut Gaze) -> Result<Box<[T]>, GazeErr> {
+//         let mut res: Vec<T> = Vec::new();
+//         for step in &*self.0 {
+//             let r = gaze.run(*step);
+//             match r {
+//                 Ok(r) => res.push(r),
+//                 Err(_) => return Err(GazeErr::NoMatch),
+//             }
+//         }
+//         Ok(res.into_boxed_slice())
+//     }
+// }
